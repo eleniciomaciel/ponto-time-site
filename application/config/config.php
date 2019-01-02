cfb626f6c90e09f9524e0c1532d40c92e27942a1
Elenicio Maciel de Souza <eleniciosouza@gmail.com>
2019-01-02 17:31:36 -0300

    adicionando o stilus

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/c8-ponto-time_NEW_MENU/system_ponto_time_demo/';
+$config['base_url'] = 'http://localhost/ponto-time-git/';
 
 /*
 |--------------------------------------------------------------------------
